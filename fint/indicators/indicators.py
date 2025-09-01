d11c315531ce8690f7b34b1877e89d27d883931c
Saul <saul@etrapi.com>
2025-09-01 13:59:48 +0200

    inidcators added to public api

--- fint/indicators/indicators.py
+++ fint/indicators/indicators.py
@@ -11,3 +11,24 @@ def SMA(x, period, backend=None):
 
 def RSI(x, period=14, backend=None):
     return get_indicator("RSI", backend)(x, period)
+
+def ROC(x, period=14, backend=None):
+    return get_indicator("ROC", backend)(x, period)
+
+def VWAP(x, period=14, backend=None):
+    return get_indicator("VWAP", backend)(x, period)
+
+def ROCOHLC(open, high, low, close, length, backend=None):
+    return get_indicator("ROCOHLC", backend)(open, high, low, close, length)
+
+def VOLUME_ACCU(volume, start_time_mask, backend=None):
+    return get_indicator("VOLUME_ACCU", backend)(volume, start_time_mask)
+
+def OPEN_PCT_CHANGE(open, close, start_time_mask, backend=None):
+    return get_indicator("OPEN_PCT_CHANGE", backend)(open, close, start_time_mask)
+
+def YESTERDAY_CLOSE_PCT_CHANGE(close, start_time_mask, backend=None):
+    return get_indicator("YESTERDAY_CLOSE_PCT_CHANGE", backend)(close, start_time_mask)
+
+def TIME_FROM_START(open, start_time_mask, backend=None):
+    return get_indicator("TIME_FROM_START", backend)(open, start_time_mask)   
