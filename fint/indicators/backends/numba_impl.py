7f9dc57c9b37960c0df8aa5d8de3964bfd78140d
Saul <saul@etrapi.com>
2025-09-01 13:47:53 +0200

    added stg727 indicators

--- fint/indicators/backends/numba_impl.py
+++ fint/indicators/backends/numba_impl.py
@@ -1,7 +1,13 @@
 import numpy as np
 from ..registry import register_indicator
 from ..io import io_wrapper, optional_numba
-from ..backends.numpy_impl import roc_ohlc_numpy
+from ..backends.numpy_impl import (
+    roc_ohlc_numpy,
+    volume_accu_indicator_numpy,
+    open_pct_change_numpy,
+    yesterday_close_pct_change_numpy,
+    time_from_start_mask_numpy
+)
 
 def sma_numba(x: np.ndarray, period: int) -> np.ndarray:
     out = np.empty_like(x, dtype=np.float64)
@@ -41,7 +47,31 @@ def roc_ohlc_numba(
     ''' Equivalente a roc_ohlc_numpy '''
     return roc_ohlc_numpy(open, high, low, close, length)
 
+def volume_accu_indicator_numba(volume: np.ndarray, start_time_mask: np.ndarray):
+    ''' Equivalente a volume_accu_indicator_numpy '''
+    return volume_accu_indicator_numpy(volume, start_time_mask)
+
+def open_pct_change_numba(
+    open: np.ndarray, close: np.ndarray, start_time_mask: np.ndarray
+):
+    ''' Equivalente a open_pct_change_numpy '''
+    return open_pct_change_numpy(open, close, start_time_mask)
+
+def yesterday_close_pct_change_numba(
+    close: np.ndarray, start_time_mask: np.ndarray
+):
+    ''' Equivalente a yesterday_close_pct_change_numpy '''
+    return yesterday_close_pct_change_numpy(close, start_time_mask)
+
+def time_from_start_mask_numba(open: np.ndarray, start_time_mask: np.ndarray):
+    ''' Equivalente a time_from_start_mask_numpy '''
+    return time_from_start_mask_numpy(open, start_time_mask)
+
 # Registro dinámico
 register_indicator("SMA", "numba", io_wrapper(optional_numba(sma_numba)))
 register_indicator("RSI", "numba", io_wrapper(optional_numba(rsi_numba)))
 register_indicator("ROCOHLC", "numba", io_wrapper(optional_numba(roc_ohlc_numba)))
+register_indicator("VOLUME_ACCU", "numba", io_wrapper(optional_numba(volume_accu_indicator_numba)))
+register_indicator("OPEN_PCT_CHANGE", "numba", io_wrapper(optional_numba(open_pct_change_numba)))
+register_indicator("YESTERDAY_CLOSE_PCT_CHANGE", "numba", io_wrapper(optional_numba(yesterday_close_pct_change_numba)))
+register_indicator("TIME_FROM_START", "numba", io_wrapper(optional_numba(time_from_start_mask_numba)))
--- fint/indicators/backends/numpy_impl.py
+++ fint/indicators/backends/numpy_impl.py
@@ -52,9 +52,76 @@ def roc_ohlc_numpy(
     indicator[length:, :] = roc
     return indicator
 
+def volume_accu_indicator_numpy(volume: np.ndarray, start_time_mask: np.ndarray):
+    volume_acu = np.full_like(volume, np.nan)
+    for column_index in range(volume.shape[1]):
+        for row_index in range(volume.shape[0]):
+            if start_time_mask[row_index,column_index]:# added column_¡ndex from original
+                volume_acu[row_index, column_index] = volume[row_index, column_index]
+            else:
+                volume_acu[row_index, column_index] = (
+                    volume_acu[row_index - 1, column_index]
+                    + volume[row_index, column_index]
+                )
+    return volume_acu
+
+def open_pct_change_numpy(
+    open: np.ndarray, close: np.ndarray, start_time_mask: np.ndarray
+):
+    open_pct_change = np.full_like(close, np.nan)
+    open_market_row = 0
+    for column_index in range(open.shape[1]):
+        for row_index in range(open.shape[0]):
+            if start_time_mask[row_index, column_index]:# added column_¡ndex from original
+                open_pct_change[row_index, column_index] = 0
+                open_market_row = row_index
+            else:
+                close_now = close[row_index, column_index]
+                open_market_start = open[open_market_row, column_index]
+                open_pct_change[row_index, column_index] = (
+                    (close_now - open_market_start) / open_market_start
+                )
+        open_market_row = 0
+    return open_pct_change
+
+# yesterday rth close pct change
+def yesterday_close_pct_change_numpy(
+    close: np.ndarray, start_time_mask: np.ndarray
+): 
+    close_pct_change = np.full_like(close, np.nan)
+    close_market_row = 0
+    for column_index in range(close.shape[1]):
+        for row_index in range(close.shape[0]):
+            if start_time_mask[row_index, column_index]:# added column_¡ndex from original
+                close_pct_change[row_index, column_index] = 0
+                close_market_row = row_index
+            else:
+                close_last= close[-1, column_index]
+                close_yesterday = close[close_market_row, column_index]
+                close_pct_change[row_index, column_index] = (
+                    (close_last - close_yesterday) / close_yesterday
+                )
+    return close_pct_change
+
+
+def time_from_start_mask_numpy(open: np.ndarray, start_time_mask: np.ndarray):
+    time_from_start = np.full_like(open, np.nan)
+    for column_index in range(open.shape[1]):
+        for row_index in range(open.shape[0]):
+            if start_time_mask[row_index, column_index]: # added column_¡ndex from original
+                time_from_start[row_index, column_index] = 0
+            else:
+                time_from_start[row_index, column_index] = time_from_start[row_index - 1, column_index] + 1
+    return time_from_start
+
+
 
 # Registro dinámico
 register_indicator("SMA", "numpy", io_wrapper(sma_numpy))
 register_indicator("RSI", "numpy", io_wrapper(rsi_numpy))
 register_indicator("VWAP", "numpy", io_wrapper(vwap_numpy))
 register_indicator("ROCOHLC", "numpy", io_wrapper(roc_ohlc_numpy))
+register_indicator("VOLUME_ACCU", "numpy", io_wrapper(volume_accu_indicator_numpy))
+register_indicator("OPEN_PCT_CHANGE", "numpy", io_wrapper(open_pct_change_numpy))
+register_indicator("YESTERDAY_CLOSE_PCT_CHANGE", "numpy", io_wrapper(yesterday_close_pct_change_numpy))
+register_indicator("TIME_FROM_START", "numpy", io_wrapper(time_from_start_mask_numpy))
